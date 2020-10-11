cbcfc95b899345440e3c0623e463d7171ceca9de
GeneralBodkhi <65399957+GeneralBodkhi@users.noreply.github.com>
2020-10-11 15:32:39 +0300

    1.js
    12

---
+++ name.js
@@ -0,0 +1,23 @@
+var dogName = "Maxim"
+var dogAge = 23
+if (dogAge > 20){
+console.write(dogName + " say WOOF WOOF);
+} else {
+console.write(dogName + " say woof woof);
+}
+
+var dogName = "Circle"
+var dogAge = 17
+if (dogAge > 20){
+console.write(dogName + " say WOOF WOOF);
+} else {
+console.write(dogName + " say woof woof);
+}
+
+var dogName = "Jack"
+var dogAge = 12
+if (dogAge > 20){
+console.write(dogName + " say WOOF WOOF);
+} else {
+console.write(dogName + " say woof woof);
+}
